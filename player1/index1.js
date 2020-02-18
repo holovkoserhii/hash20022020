120362e8224805132f4b48a5aa49760bd5ba6511
holovs01 <serhii_holovko1@epam.com>
2020-02-18 21:56:32 +0200

    initial commit

---
+++ player1/index1.js
@@ -0,0 +1,28 @@
+var fileName;
+
+function readFile() {
+  var file = document.getElementById("file").files[0];
+  fileName = file.name.slice(0, file.name.length - 3);
+  const reader = new FileReader();
+  reader.onload = function(event) {
+    const contents = event.target.result
+      .trim()
+      .replace(/\n/g, ",")
+      .split(",");
+    // const fullField = arraysOfObjects(contents);
+    startAnalysis(contents);
+    return;
+  };
+  reader.readAsText(file);
+}
+
+function downloadCsv(arr, name) {
+  let newFileContent = `${arr.length}\n`;
+  newFileContent += arr.join(" ");
+
+  let hiddenElement = document.createElement("a");
+  hiddenElement.href = "data:text;charset=utf-8," + encodeURI(newFileContent);
+  hiddenElement.target = "_blank";
+  hiddenElement.download = `${name}.txt`;
+  hiddenElement.click();
+}
---
+++ player2/index2.js
@@ -0,0 +1,5 @@
+const startAnalysis = a => {
+  console.log(a);
+  const answer = [0, 1, 7];
+  downloadCsv(answer, fileName);
+};
