056fac71210da6826acbb5f0e16f7646902fe085
exypb36 <oleksandr.bereziuk1@mckesson.com>
2020-02-20 23:08:05 +0200

    Player 2 commit

--- player2/index2.js
+++ player2/index2.js
@@ -2,6 +2,7 @@
 
 const startAnalysis = a => {
   let { books, daysToScan, libraries } = a;
+  libraries.splice(0,1);
   let totalScore = 0;
   let signUpContainer = 0;
   let output = [];
@@ -13,6 +14,7 @@ const startAnalysis = a => {
     
     if ( time > 0 ) {
       booksToScan = lib.bookIds.slice(0, time*lib.shipBooksPerDay-1 );
+      booksToScan = sortArr(books, booksToScan);
       booksToScan.forEach( scan => {
         totalScore += books.find( book => book.id+'' === scan ).score;
       } );
@@ -23,6 +25,26 @@ const startAnalysis = a => {
     }
 
   } );
-  console.log(totalScore);
+  console.log(totalScore)
   return output;
 };
+
+function sortArr(books, bookIds) {
+  let coincidence = 0;
+  while (coincidence > 0) {
+    coincidence=0;
+    for (let i=0; i<bookIds.length; i++) {
+      bookIds[i]
+      if (bookIds[i+1]) {
+        if (books.find( book => book.id === +bookIds[i] ) < books.find( book => book.id === +bookIds[i+1] )) {
+          coincidence++;
+          let temp = bookIds[i];
+          bookIds[i] = bookIds[i+1];
+          bookIds[i+1] = temp;
+        }
+      }
+    }
+  }
+  
+  return bookIds;
+}
