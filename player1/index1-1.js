a1cf9c88f5ffb11ea5429ef5864af6db849bc361
holovs01 <serhii_holovko1@epam.com>
2020-02-20 21:47:19 +0200

    w

--- player1/index1-1.js
+++ player1/index1-1.js
@@ -16,16 +16,62 @@ const file_f_url =
 
 var fileName;
 
+const getData = contentArray => {
+  if (contentArray[contentArray.length - 1] === "") {
+    contentArray.pop();
+  }
+  const bookScores = contentArray[1].split(" ");
+  const books = bookScores.map((score, id) => ({
+    id: String(id),
+    score: parseInt(score, 10)
+  }));
+  const daysToScan = +contentArray[0].split(" ")[2];
+  const libsRaw = [];
+  for (let i = 2; i < contentArray.length; i += 2) {
+    libsRaw.push(`${contentArray[i]} ${contentArray[i + 1]}`);
+  }
+  if (libsRaw[libsRaw.length - 1].split(" ").length < 3) {
+    libsRaw.pop();
+  }
+  const unsortedLibraries = libsRaw.reduce((accum, elem, index) => {
+    const rawLib = elem.split(" ");
+    const libId = index;
+    const shipBooksPerDay = +rawLib[2];
+    let signUpDays = +rawLib[1];
+    if (signUpDays === 0) {
+      signUpDays = 0.001;
+    }
+    rawLib.splice(0, 3);
+    const bookIds = rawLib;
+    const importance1 = (shipBooksPerDay * bookIds.length) / signUpDays;
+
+    const lib = { libId, shipBooksPerDay, signUpDays, bookIds, importance1 };
+    accum.push(lib);
+    return accum;
+  }, []);
+  const libraries = unsortedLibraries.sort((a, b) => {
+    if (a.importance1 > b.importance1) {
+      return -1;
+    }
+    if (a.importance1 < b.importance1) {
+      return 1;
+    }
+    return 0;
+  });
+  return { libraries, daysToScan, books };
+};
+
 async function readFile() {
-  const fileToProcess = file_a_url;
+  const fileToProcess = file_c_url;
   const file = await axios.get(fileToProcess);
   fileName = fileToProcess
     .split("/")
     [fileToProcess.split("/").length - 1].split(".")[0];
 
   const content = file.data.split("\n");
-  console.log(content);
-  // startAnalysis(content);
+  const dataRaw = getData(content);
+  // console.log(dataRaw);
+  startAnalysis(dataRaw);
 }
 
 function downloadCsv(arr, name) {
