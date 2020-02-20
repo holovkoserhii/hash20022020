c9c8e2586d1f28b399d1abf1661cb1dfe45e3608
exypb36 <oleksandr.bereziuk1@mckesson.com>
2020-02-20 22:41:16 +0200

    Player 2 commit

--- player2/index2.js
+++ player2/index2.js
@@ -1,8 +1,22 @@
+
+
 const startAnalysis = a => {
-  // console.log(a);
-  // const answer = [0, 1, 7];
-  // downloadCsv(answer, fileName);
+  let { books, daysToScan, libraries } = a;
+  libraries.splice(0,1);
+  let result = 0;
+  let signUpContainer = 0;
+
+  libraries.forEach( lib => {
+    signUpContainer += lib.signUpDays;
+    let time = daysToScan - signUpContainer;
+    
+    if ( time > 0 ) {
+      booksToScan = lib.bookIds.slice(0, time*lib.shipBooksPerDay-1 );
+      booksToScan.forEach( scan => {
+        result += books.find( book => book.id+'' === scan ).score;
+      } )
+    }
+  } );
+
+  return console.log(result);
 };
-
-
-console.log('Hello HASH')
